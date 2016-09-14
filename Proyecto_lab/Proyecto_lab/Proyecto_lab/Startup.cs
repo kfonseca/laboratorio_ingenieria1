a3cb0d644b391d8a8794ef30d2f781eccdbde043
kfonseca <kfonseca.hp@gmail.com>
2016-09-14 12:09:54 -0600

    Laboratorio 1 Ing. Software

---
+++ Proyecto_lab/Proyecto_lab/Proyecto_lab/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_lab.Startup))]
+namespace Proyecto_lab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
