cb50b1afff2c5973cc8308901136ea6adaf55fdb
kfonseca <kfonseca.hp@gmail.com>
2016-09-16 14:02:47 -0600

    Falta solo punto 8

---
+++ Proyecto_lab/Proyecto_lab/Proyecto_lab/Controllers/Lab_IngIController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_lab.Models;
+
+namespace Proyecto_lab.Controllers
+{
+    public class Lab_IngIController : Controller
+    {
+        BD_B02441Entities baseDatos = new BD_B02441Entities();
+        // GET: Lab_IngI
+        public ActionResult Index()
+        {
+            ModeloIntermedio modelo = new ModeloIntermedio();
+            modelo.listaClientes = baseDatos.Cliente.ToList();
+            modelo.listaTelefonos = baseDatos.Telefono.ToList();
+            modelo.listaCuentas = baseDatos.Cuenta.ToList();       
+            return View(modelo);
+        }
+        public ActionResult Create() {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ModeloIntermedio modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                baseDatos.Cliente.Add(modelo.modeloCliente);
+                baseDatos.SaveChanges();
+                if (modelo.telefono1.numero != null)
+                {
+                    modelo.telefono1.cedula = modelo.modeloCliente.cedula;
+                    baseDatos.Telefono.Add(modelo.telefono1);
+                }
+                if (modelo.telefono2.numero != null)
+                {
+                    modelo.telefono2.cedula = modelo.modeloCliente.cedula;
+                    baseDatos.Telefono.Add(modelo.telefono2);
+                }
+                baseDatos.SaveChanges();
+
+                if (modelo.cuenta1.numero != null)
+                {
+                    modelo.cuenta1.cedula = modelo.modeloCliente.cedula;
+                    baseDatos.Cuenta.Add(modelo.cuenta1);
+                }
+                if (modelo.cuenta2.numero != null)
+                {
+                    modelo.cuenta2.cedula = modelo.modeloCliente.cedula;
+                    baseDatos.Cuenta.Add(modelo.cuenta2);
+                }
+                if (modelo.cuenta3.numero != null)
+                {
+                    modelo.cuenta3.cedula = modelo.modeloCliente.cedula;
+                    baseDatos.Cuenta.Add(modelo.cuenta3);
+                }
+                baseDatos.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Debe completar toda la información necesaria.");
+                return View(modelo);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Proyecto_lab/Proyecto_lab/Proyecto_lab/Models/ModeloIntermedio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_lab.Models
+{
+    public class ModeloIntermedio
+    {
+        public Cliente modeloCliente { get; set; }
+        public Telefono telefono1 { get; set;  }
+        public Telefono telefono2 { get; set; }
+        public Cuenta cuenta1 { get; set; }
+        public Cuenta cuenta2 { get; set; }
+        public Cuenta cuenta3 { get; set; }
+
+        public List<Cliente> listaClientes = new List<Cliente>();
+        public List<Telefono> listaTelefonos = new List<Telefono>();
+        public List<Cuenta> listaCuentas = new List<Cuenta>(); 
+    }
+}
